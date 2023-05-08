b7f79e245b2ad3fbf6150f6f1ebe184404e37088
Anton <amurawew@yandex.ru>
2023-05-08 20:16:14 +0300

    Create model class, delete control methods

--- lingvforecast/ling_methods.py
+++ lingvforecast/ling_methods.py
@@ -12,140 +12,69 @@ from typing import Sequence
 CrudeForecast = namedtuple(
     'CrudeForecast', ['corrdata', 'fordata', 'fulldata', 'corrcoef'])
 
-Forecast = namedtuple('Forecast', ['forecast', 'info'])
-
-# control
+# Forecast = namedtuple('Forecast', ['forecast', 'info'])
 
 
-def ling_control(crow, prow, ctrl_rows, prdt_rows, alpha_coef=0.95, beta_coef=0.95, hmethod=None):
-    '''
-    crow - (control row) row with which control is made.
-    prow - (prediction row) row to be predicted.
-    ctrl_rows - array of arrays, control rows
-    prdt_rows - array of arrays, prediction rows
-    alpha_coef - [-1; 1] search coefficient to find parts in ctrl_rows similar to crow 
-    beta_cpef - [-1; 1] search coefficient to find parts in prdt_rows similar to prow 
-    '''
-    assert len(crow) > len(
-        prow), "Control row must be longer than prediction row, because it contains next values."
-    assert len(ctrl_rows) == len(
-        prdt_rows), "Arrays must have equals dimension!"
+class TopForecast:
 
-    assert not np.isscalar(ctrl_rows) or not np.isscalar(
-        ctrl_rows[0]), "frows - array of arrays!"
+    def __init__(self, top):
+        self._crude_forecast = [None for _ in range(top)]
 
-    assert not np.isscalar(prdt_rows) or not np.isscalar(
-        prdt_rows[0]), "frows - array of arrays!"
+    def append(self, el: CrudeForecast):
+        assert isinstance(
+            el, CrudeForecast), 'el - object of CrudeForecast class!'
+        self._add_top_corr(self._crude_forecast, el)
 
-    for i in range(len(ctrl_rows)):
-        assert len(ctrl_rows[i]) == len(
-            prdt_rows[i]), "Arrays must have equals dimension!"
+    def get_result(self):
+        res = []
+        for val in self._crude_forecast:
+            if not val is None:
+                res.append(val)
+        return res
 
-    assert -1 <= alpha_coef <= 1, "Correlation coefficient have error value!"
-    assert -1 <= beta_coef <= 1, "Correlation coefficient have error value!"
+    def __str__(self) -> str:
+        return str(self._crude_forecast)
 
-    if not hmethod:
-        hmethod = proc_methods.median
+    def __repr__(self) -> str:
+        return str(self._crude_forecast)
 
-    full_len = len(crow)
-    sub_len = len(prow)
+    def __iter__(self):
+        return iter(self.get_result())
 
-    result = []
+    @staticmethod
+    def _add_top_corr(values, new_val):
+        '''
 
-    for i in range(len(ctrl_rows)):
-        # get a row at this step for control and predict
-        ctrl_r, prdt_r = ctrl_rows[i], prdt_rows[i]
+        values - array type content 'Forecast' and 'None' objects.
+        new_val - object of 'Forecast' class
+        '''
+        # if array content empty place
+        # just insert in empty place
+        for i, val in enumerate(values):
+            if val is None:
+                values[i] = new_val
+                return values
 
-        for j in range(len(ctrl_r) - full_len + 1):
-            # get part of control series
-            c_part = ctrl_r[j:j+full_len]
-            # crow longer than prow
-            c_coef = row_corr(crow, c_part)
+        # if array dont content empty place (None objects)
+        # insert new item at place where old item corrcoef less then new item corrcoef
+        min_coef = values[0].corrcoef
+        index = 0
+        for i, val in enumerate(values):
+            if val.corrcoef < min_coef:
+                min_coef = val.corrcoef
+                index = i
 
-            if c_coef >= alpha_coef:
-                # get part of prediction series
-                p_part = prdt_r[j:j+full_len]
-                # a correlation calculate
-                p_coef = row_corr(prow, p_part[:sub_len])
+        if min_coef < new_val.corrcoef:
+            values[index] = new_val
 
-                if p_coef >= beta_coef:
-                    # if all conditions done
-                    result.append(ling_method(prow, p_part))
+        # without any difference
+        return values
 
-    # calculate final value
-    return hmethod(result)
-
-
-def ling_control_top(crow, prow, ctrl_rows, prdt_rows, alpha_coef=0.5, beta_coef=0.5, top=99, hmethod=None):
-    '''
-    crow - (control row) row with which control is made.
-    prow - (prediction row) row to be predicted.
-    ctrl_rows - array of arrays, control rows
-    prdt_rows - array of arrays, prediction rows
-    alpha_coef - [-1; 1] search coefficient to find parts in ctrl_rows similar to crow 
-    beta_cpef - [-1; 1] search coefficient to find parts in prdt_rows similar to prow 
-    '''
-    assert len(crow) > len(
-        prow), "Control row must be longer than prediction row, because it contains next values."
-    assert len(ctrl_rows) == len(
-        prdt_rows), "Arrays must have equals dimension!"
-
-    assert not np.isscalar(ctrl_rows) or not np.isscalar(
-        ctrl_rows[0]), "frows - array of arrays!"
-
-    assert not np.isscalar(prdt_rows) or not np.isscalar(
-        prdt_rows[0]), "frows - array of arrays!"
-
-    for i in range(len(ctrl_rows)):
-        assert len(ctrl_rows[i]) == len(
-            prdt_rows[i]), "Arrays must have equals dimension!"
-
-    assert -1 <= alpha_coef <= 1, "Correlation coefficient have error value!"
-    assert -1 <= beta_coef <= 1, "Correlation coefficient have error value!"
-
-    if not hmethod:
-        hmethod = proc_methods.median
-
-    full_len = len(crow)
-    sub_len = len(prow)
-
-    result = []
-
-    crude_control = TopForecast(top)
-    for i in range(len(ctrl_rows)):
-        # get a row at this step for control and predict
-        ctrl_r, prdt_r = ctrl_rows[i], prdt_rows[i]
-
-        for j in range(len(ctrl_r) - full_len + 1):
-            # get part of control series
-            c_part = ctrl_r[j:j+full_len]
-            # get part of prediction series
-            p_part = prdt_r[j:j+full_len]
-            # crow longer than prow
-            c_coef = row_corr(crow, c_part)
-
-            if c_coef >= alpha_coef:
-                el = CrudeForecast(c_part, p_part, None, c_coef)
-                crude_control.append(el)
-
-    for el in crude_control.get_result():
-        if row_corr(el.fordata[:sub_len], prow) >= beta_coef:
-            result.append(ling_method(prow, el.fordata))
-
-    # calculate final value
-    return hmethod(result)
-
-
-def ling_control_frame():
-    '''
-
-    '''
-    pass
 
 # forecast
 
 
-def ling_predict(prow, frows, corrcoef=0.95, fcount=1, hmethod=None, normalize=False):
+def ling_predict(prow, frows, corrcoef=0.95, horizon=1, hmethod=None, normalize=False):
     '''
     prow - row to be predicted
 
@@ -159,7 +88,7 @@ def ling_predict(prow, frows, corrcoef=0.95, fcount=1, hmethod=None, normalize=F
 
     normalize - parametr that provide manipulation with normalize forecast (not with result)
 
-    fcount - if fcount equals 1 return one value if great then 1 return array
+    horizon - if horizon equals 1 return one value if great then 1 return array
     of values.
     '''
 
@@ -177,16 +106,16 @@ def ling_predict(prow, frows, corrcoef=0.95, fcount=1, hmethod=None, normalize=F
     else:
         prow_arr = prow
     # forecast array
-    res_forecast = np.zeros(fcount)
+    res_forecast = np.zeros(horizon)
 
     # method is used with NORMALIZE forecast values
     if normalize:
-        for i in range(fcount):
+        for i in range(horizon):
             forecast = []
             # additional information
             add_info = {}
 
-            for el in find_all_rows(prow_arr, frows, corrcoef=corrcoef, fcount=1):
+            for el in find_all_rows(prow_arr, frows, corrcoef=corrcoef, horizon=1):
                 # normalize y-row
                 fr = _normalize(el.fordata, np.mean(
                     el.corrdata), np.var(el.corrdata))[0]
@@ -201,13 +130,13 @@ def ling_predict(prow, frows, corrcoef=0.95, fcount=1, hmethod=None, normalize=F
 
     # method is used with FINISH forecast values
     else:
-        for i in range(fcount):
+        for i in range(horizon):
             forecast = []
             # additional information
             add_info = {}
 
             # predict next value
-            for el in find_all_rows(prow_arr, frows, corrcoef=corrcoef, fcount=1):
+            for el in find_all_rows(prow_arr, frows, corrcoef=corrcoef, horizon=1):
                 fr = ling_method(prow_arr, el.fulldata)
                 add_info[id(fr)] = el
                 forecast.append(fr)
@@ -217,13 +146,13 @@ def ling_predict(prow, frows, corrcoef=0.95, fcount=1, hmethod=None, normalize=F
             # create new prow
             prow_arr = np.append(prow_arr[1:], [res_forecast[i]])
 
-    if fcount == 1:
+    if horizon == 1:
         return res_forecast[0]
     else:
         return res_forecast
 
 
-def ling_predict_top(prow, frows, corrcoef=0.5, top=99, fcount=1, hmethod=None, normalize=False):
+def ling_predict_top(prow, frows, corrcoef=0.5, top=99, horizon=1, hmethod=None, normalize=False):
     '''
     Use just `top` values to calculate result.
 
@@ -241,7 +170,7 @@ def ling_predict_top(prow, frows, corrcoef=0.5, top=99, fcount=1, hmethod=None, 
 
     normalize - parametr that provide manipulation with normalize forecast (not with result)
 
-    fcount - if fcount equals 1 return one value if great then 1 return array
+    horizon - if horizon equals 1 return one value if great then 1 return array
     of values.
     '''
 
@@ -261,12 +190,12 @@ def ling_predict_top(prow, frows, corrcoef=0.5, top=99, fcount=1, hmethod=None, 
         prow_arr = np.array(prow)
     else:
         prow_arr = prow
-    # forecast array with fcount steps
-    res_forecast = np.zeros(fcount)
+    # forecast array with horizon steps
+    res_forecast = np.zeros(horizon)
 
-    for j in range(fcount):
+    for j in range(horizon):
         crude_forecast = TopForecast(top)
-        for el in find_all_rows(prow_arr, frows, corrcoef=corrcoef, fcount=1):
+        for el in find_all_rows(prow_arr, frows, corrcoef=corrcoef, horizon=1):
             crude_forecast.append(el)
 
         crude_forecast_result = crude_forecast.get_result()
@@ -292,7 +221,7 @@ def ling_predict_top(prow, frows, corrcoef=0.5, top=99, fcount=1, hmethod=None, 
         # create new prow
         prow_arr = np.append(prow_arr[1:], [res_forecast[j]])
 
-    if fcount == 1:
+    if horizon == 1:
         return res_forecast[0]
     else:
         return res_forecast
@@ -330,7 +259,7 @@ def ling_predict_frame(prows, frows, indices, corrcoef=0.5, top=99, normalize=Fa
 
     for prow_arr in prows:
         crude_forecast = TopForecast(top)
-        for el in find_all_rows(prow_arr, frows, corrcoef=corrcoef, fcount=1):
+        for el in find_all_rows(prow_arr, frows, corrcoef=corrcoef, horizon=1):
             crude_forecast.append(el)
 
         # sort predicted values. At the beginning there will be elements with the maximum correlation coefficient
@@ -397,7 +326,7 @@ def ling_corr_frame(prows, frows, indices, corrcoef=0.5, top=99):
 
         # collect all the best subrows by correlation
         refine_forecast = TopForecast(top)
-        for el in find_all_rows(prow_arr, frows, corrcoef=corrcoef, fcount=1):
+        for el in find_all_rows(prow_arr, frows, corrcoef=corrcoef, horizon=1):
             refine_forecast.append(el)
 
         # sort values. At the beginning there will be elements with the maximum correlation coefficient
@@ -421,53 +350,6 @@ def ling_corr_frame(prows, frows, indices, corrcoef=0.5, top=99):
 
 
 # neurolinguistic methods
-
-
-class TopForecast:
-
-    def __init__(self, top):
-        self._crude_forecast = [None for _ in range(top)]
-
-    def append(self, el: CrudeForecast):
-        assert isinstance(
-            el, CrudeForecast), 'el - object of CrudeForecast class!'
-        self._add_top_corr(self._crude_forecast, el)
-
-    def get_result(self):
-        res = []
-        for val in self._crude_forecast:
-            if not val is None:
-                res.append(val)
-        return res
-
-    @staticmethod
-    def _add_top_corr(values, new_val):
-        '''
-
-        values - array type content 'Forecast' and 'None' objects.
-        new_val - object of 'Forecast' class
-        '''
-        # if array content empty place
-        # just insert in empty place
-        for i, val in enumerate(values):
-            if val is None:
-                values[i] = new_val
-                return values
-
-        # if array dont content empty place (None objects)
-        # insert new item at place where old item corrcoef less then new item corrcoef
-        min_coef = values[0].corrcoef
-        index = 0
-        for i, val in enumerate(values):
-            if val.corrcoef < min_coef:
-                min_coef = val.corrcoef
-                index = i
-
-        if min_coef < new_val.corrcoef:
-            values[index] = new_val
-
-        # without any difference
-        return values
 
 
 def _create_frame_row(forecast, slices):
@@ -506,9 +388,9 @@ def axis_reflection(arr):
     return rev_arr, inv_arr, rev_inv_arr
 
 
-def find_all_rows(crow, srows, corrcoef=0.95, fcount=1):
+def find_all_rows(crow, srows, corrcoef=0.95, horizon=1):
     '''
-    On each step return `fcount` elements if previous elements
+    On each step return `horizon` elements if previous elements
     have correlation coefficient with `crow` great then `corrcoef`.
 
     Parameters
@@ -522,7 +404,7 @@ def find_all_rows(crow, srows, corrcoef=0.95, fcount=1):
 
     corrcoef: correlation coefficient
 
-    fcount - count of forecasts (on one step, on two steps, ...)
+    horizon - count of forecasts (on one step, on two steps, ...)
     '''
     assert -1 <= corrcoef <= 1, "Incorrect corrcoef!"
 
@@ -531,15 +413,15 @@ def find_all_rows(crow, srows, corrcoef=0.95, fcount=1):
     if not len(crow) or not len(srows):
         return
 
-    # len subrow without fcount elements
+    # len subrow without horizon elements
     sub_len = len(crow)
-    # len subrow with fcount elements
-    full_len = sub_len + fcount
+    # len subrow with horizon elements
+    full_len = sub_len + horizon
 
-    # return correlation row with fcount elements at the end on each step
+    # return correlation row with horizon elements at the end on each step
     for row in srows:
         for i in range(len(row) - full_len + 1):
-            # get full subrow (with fcount elements)
+            # get full subrow (with horizon elements)
             sub_row = row[i:i+full_len]
             # get only correlation row
             corr_row = row[i:i+sub_len]
@@ -548,8 +430,8 @@ def find_all_rows(crow, srows, corrcoef=0.95, fcount=1):
             coef = row_corr(corr_row, crow)
             # if corr_row is suitable
             if coef >= corrcoef:
-                # subrow[-fcount:] - [fcount elements]
-                yield CrudeForecast(corr_row, sub_row[-fcount:], sub_row, coef)
+                # subrow[-horizon:] - [horizon elements]
+                yield CrudeForecast(corr_row, sub_row[-horizon:], sub_row, coef)
 
 
 def _normalize(row_x, Mx, Dx):
--- lingvforecast/models.py
+++ lingvforecast/models.py
@@ -3,27 +3,58 @@
 '''
 import pandas as pd
 import numpy as np
-from .ling_methods import ling_predict
+from .ling_methods import ling_method, find_all_rows, CrudeForecast, TopForecast
 from typing import Sequence
 
 
 class LingvForecast:
 
     def __init__(self) -> None:
-        self._data = []
+        # self._data = []
+        pass
 
-    def fit(self, *data, top=1):
+    def fit(self, *data, method='top', top=1, corrcoef=0.99):
         '''
         :param data: array[array]
+
+        :param method: str, 'top'/'corrcoef'
 
         Тут можно сделать преобразование данных
         для ускорения поиска результатов (оптимизация).
         '''
         assert top >= 1
-        if top == 1:
-            pass
 
-        
+        self._data = data
+        self._method = method
+        self._top = top
+        self._corrcoef = corrcoef
 
     def predict(self, prow: Sequence, horizon: int = 1):
-        pass
+        '''
+
+        :return array of arrays
+        '''
+        if self._method == 'top':
+            # TopForecast contains CrudeForecast array
+            top_correlation_rows = TopForecast(self._top)
+        elif self._method == 'corrcoef':
+            top_correlation_rows = []
+
+        # --- first step - find patterns
+        # collect all the best subrows by correlation
+        for el in find_all_rows(prow, self._data, corrcoef=self._corrcoef, horizon=horizon):
+            top_correlation_rows.append(el)
+
+        print('--- logs ---', top_correlation_rows)
+
+        # sort values. At the beginning there will be elements with the maximum correlation coefficient
+        # when calculating the correlation, the last element is removed, because it is an expert value
+        # sort_refine_forecast = sorted(
+        #     refine_forecast.get_result(), reverse=True, key=lambda val: row_corr(val.corrdata[:-1], corr_row))
+
+        # --- second step - calculate forecast
+        forecast = []
+        for cr in top_correlation_rows:
+            forecast.append(ling_method(prow, cr.fulldata))
+
+        return forecast
